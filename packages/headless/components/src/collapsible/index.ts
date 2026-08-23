a455d6058361a4c0a454da698f84bdb4cc9f3dd0
Jack Shelton <me@jackshelton.com>
2026-08-22 19:41:30 -0500

    fable-opus headless-pilot-2026-08-22/U104-collapsible attempt 2 (partial)

---
+++ packages/headless/components/src/collapsible/index.ts
@@ -0,0 +1,10 @@
+export {
+	CollapsibleContent as content,
+	CollapsibleRoot as root,
+	CollapsibleTrigger as trigger,
+} from './collapsible.tsrx';
+export type {
+	CollapsibleContentProps,
+	CollapsibleRootProps,
+	CollapsibleTriggerProps,
+} from './collapsible-types.ts';
